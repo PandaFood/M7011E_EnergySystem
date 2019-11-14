a07541e23cb7399979ad79b2aef490c5b27bca12
PandaFood <Jonathan.Brorsson@gmail.com>
2019-11-14 10:46:48 +0100

    Api with swagger

--- Simulator/app.js
+++ Simulator/app.js
@@ -3,12 +3,32 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/api');
+const apiRouter = require('./routes/api');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
-const db = require('./postgres/database');
 
 const app = express();
+
+// swagger definition
+const swaggerDefinition = {
+	info: {
+		title: 'Node Swagger API',
+		version: '1.0.0',
+		description: 'Demonstrating how to describe a RESTful API with Swagger',
+	},
+	host: 'localhost:3000',
+	basePath: '/',
+};
+
+const options = {
+	swaggerDefinition: swaggerDefinition,
+	apis: ['./routes/*.js'],
+};
+
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,8 +36,13 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/api', usersRouter);
+app.use('/api', apiRouter);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get('/swagger.json', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(swaggerSpec);
+});
+
 
 
 module.exports = app;
--- Simulator/routes/api.js
+++ Simulator/routes/api.js
@@ -1,14 +1,49 @@
 const express = require('express');
 const router = express.Router();
 
-const Simulator = require("../simulation/simulator");
+const Simulator = require('../simulation/simulator');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
 
-    Simulator.runSimulation();
+/**
+ * @swagger
+ * definitions:
+ *   House:
+ *     properties:
+ *       name:
+ *         type: string
+ *       breed:
+ *         type: string
+ *       age:
+ *         type: integer
+ *       sex:
+ *         type: string
+ *   Windturbine:
+ *      properties:
+ *          name:
+ *              type: string
+ *          
+ */
 
-    res.send('respond with a resource');
+
+/**
+ * @swagger
+ * /api/resource:
+ *   get:
+ *     tags:
+ *       - Puppies
+ *     description: Returns all puppies
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of puppies
+ *         schema:
+ *           $ref: '#/definitions/Puppy'
+ */
+router.get('/resource', function(req, res, next) {
+	//Simulator.runSimulation();
+
+	res.json('respond with a resource');
 });
 
 module.exports = router;
