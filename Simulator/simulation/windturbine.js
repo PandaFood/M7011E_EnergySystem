3adc909d668913f1a27dc9a6c9d83b9a52a1883e
97gushan <97gustavh@gmail.com>
2019-11-25 14:15:23 +0100

    Turbine breakdown (#15)
    * simulate a turbine breakdown by generating a random value and checking it against a breaking threshold
    
    * tweaking simulation values
    
    * tweak test values after changing simulation parameters

--- Simulator/simulation/windturbine.js
+++ Simulator/simulation/windturbine.js
@@ -21,8 +21,9 @@ class WindTurbine {
 
 		this.linearPowerCoeff = 10;
 		this.maxSpeed = 25.0; // maximum wind speed the turbine can work in
-		this.minSpeed = 3.5; // minimum wind peed the turbine works in
+		this.minSpeed = 3.0; // minimum wind peed the turbine works in
 		this.minOptimalSpeed = 14.0; // minimum wind speed of which the turbines produces maximum power
+		this.status = 'up';
 	}
 
 	/**
@@ -31,19 +32,17 @@ class WindTurbine {
 	 */
 	generateStartWind() {
 		const distribution = gaussian(8.0, 2);
-
 		return distribution.ppf(Math.random());
 	}
 
 	/**
-	 *  Generate the wind for the next poll in the simulation.
+	 * Generate the wind for the next poll in the simulation.
 	 * The generation is based on a Gaussian distribution with the poll time as variance
 	 * This ensures that higher times betweem polls gives a higher chanse of bigger differences in wind speed
 	 */
 	generateWind() {
 		const distribution = gaussian(this.calcOffset(), this.pollTime / 1000);
 		let wind = this.windSpeed + distribution.ppf(Math.random());
-
 		if (wind < 0) wind = 0;
 		this.windSpeed = wind;
 	}
@@ -56,7 +55,7 @@ class WindTurbine {
 	 */
 	calcPowerCoeff(windSpeed) {
 		const mean = this.minOptimalSpeed / 2 + this.minSpeed / 2;
-		const variance = this.minOptimalSpeed / 4 + this.minSpeed / 2;
+		const variance = this.minOptimalSpeed / 4 + this.minSpeed;
 
 		const distribution = gaussian(mean, variance);
 		const sigmoid = distribution.cdf(windSpeed);
@@ -73,13 +72,17 @@ class WindTurbine {
 	calcOffset() {
 		let offset = 0;
 
-		if (this.windSpeed < 5) {
-			offset = 0.075;
-		} else if (this.windSpeed > 15) {
-			if (this.windSpeed > 25) {
-				offset = -0.075;
+		if (this.windSpeed < this.minSpeed * 2) {
+			if (this.windSpeed < this.minSpeed) {
+				offset = 0.15;
 			} else {
+				offset = 0.05;
+			}
+		} else if (this.windSpeed > this.minOptimalSpeed) {
+			if (this.windSpeed > this.maxSpeed) {
 				offset = -0.05;
+			} else {
+				offset = -0.015;
 			}
 		} else {
 			offset = 0;
@@ -109,22 +112,50 @@ class WindTurbine {
 		console.log(date + ' - ' + this.id + ': ' + (this.power).toFixed(2) +
 			'Ws -- ' + this.windSpeed.toFixed(2) + 'm/s');
 	}
+	/**
+	 * Calculates a random number, if it is below a threshold and the turbine is up
+	 * then set the status to down and set a Timeout for when it should be up and running again
+	 */
+	simulateBreakdown() {
+		const ran = Math.random();
+		// The threshold where a turbines breaks, higher speeds gives a higher risk of breaking
+		const threshold = 0.0001 * this.windSpeed;
+
+		if (ran < threshold && this.status == 'up') {
+			this.status = 'down';
+
+			// values gathered by experimenting,trying to get results between 3000 and 5000 ms
+			const distribution = gaussian(4000, 500 * 10 ** 3);
+			const timeoutLength = Math.floor(distribution.ppf(Math.random()));
+
+			setTimeout(function(self) {
+				self.status = 'up';
+			}, timeoutLength, this);
+		}
+	};
 
 	/**
 	 *@param {number} deltaTime
 	 *@return {number}
 	 */
 	runSimulation(deltaTime) {
-		this.generateWind(deltaTime);
-		this.generatePower(deltaTime);
+		this.simulateBreakdown();
 
 		const pollData = {
 			id: this.id,
 			power: this.power,
 			windSpeed: this.windSpeed,
-			status: 'up',
+			status: this.status,
 			coords: this.coords,
 		};
+
+		if (this.status == 'up') {
+			this.generateWind(deltaTime);
+			this.generatePower(deltaTime);
+		} else {
+			pollData.power = 0;
+			pollData.windSpeed = 0;
+		}
 
 		return pollData;
 	}
--- Simulator/test/testwind.js
+++ Simulator/test/testwind.js
@@ -14,23 +14,23 @@ describe('Wind Turbine', function() {
 			windTurbine = new WindTurbine(0);
 			windTurbine.linearPowerCoeff = 1;
 			assert.equal(windTurbine.calcPowerCoeff(0).toFixed(2), 0);
-			assert.equal(windTurbine.calcPowerCoeff(3.5).toFixed(2), 0.01);
-			assert.equal(windTurbine.calcPowerCoeff(5).toFixed(2), 0.05);
-			assert.equal(windTurbine.calcPowerCoeff(7.5).toFixed(2), 0.29);
-			assert.equal(windTurbine.calcPowerCoeff(9).toFixed(2), 0.54);
-			assert.equal(windTurbine.calcPowerCoeff(10).toFixed(2), 0.71);
-			assert.equal(windTurbine.calcPowerCoeff(14).toFixed(2), 0.99);
-			assert.equal(windTurbine.calcPowerCoeff(15).toFixed(2), 1);
+			assert.equal(windTurbine.calcPowerCoeff(3.5).toFixed(2), 0.02);
+			assert.equal(windTurbine.calcPowerCoeff(5).toFixed(2), 0.08);
+			assert.equal(windTurbine.calcPowerCoeff(7.5).toFixed(2), 0.35);
+			assert.equal(windTurbine.calcPowerCoeff(9).toFixed(2), 0.58);
+			assert.equal(windTurbine.calcPowerCoeff(10).toFixed(2), 0.72);
+			assert.equal(windTurbine.calcPowerCoeff(14).toFixed(2), 0.98);
+			assert.equal(windTurbine.calcPowerCoeff(15).toFixed(2), 0.99);
 			assert.equal(windTurbine.calcPowerCoeff(25).toFixed(2), 1);
 		});
 	});
 
 	describe('#generatePower()', function() {
-		it('generated power should be 7.07', function() {
+		it('generated power should be 7.22', function() {
 			windTurbine = new WindTurbine(0);
 			windTurbine.windSpeed = 10;
 			windTurbine.generatePower(0.1);
-			assert.equal(windTurbine.power.toFixed(2), 7.07);
+			assert.equal(windTurbine.power.toFixed(2), 7.22);
 		});
 
 		it('speeds lower then minimum speed or higher then maximum should give 0', function() {
