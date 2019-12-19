99218cd8f5f84433afe48a01a6befe69c94a57dd
PandaFood <Jonathan.Brorsson@gmail.com>
2019-12-19 11:36:58 +0100

    Added login to router

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -8,22 +8,67 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    },
   },
   {
     path: '/about',
     name: 'about',
+    meta: {
+      requiresAuth: true
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    // TODO: Validate JWT for simple time
+      if (localStorage.getItem('jwt') == null) {
+          next({
+              path: '/login',
+              params: { nextUrl: to.fullPath }
+          })
+      } else {
+          let user = JSON.parse(localStorage.getItem('user'))
+          if(to.matched.some(record => record.meta.is_admin)) {
+              if(user.is_admin == 1){
+                  next()
+              }
+              else{
+                  next({ name: 'userboard'})
+              }
+          }else {
+              next()
+          }
+      }
+  } else if(to.matched.some(record => record.meta.guest)) {
+      if(localStorage.getItem('jwt') == null){
+          next()
+      }
+      else{
+          next({ name: 'userboard'})
+      }
+  }else {
+      next() 
+  }
 })
 
 export default router
